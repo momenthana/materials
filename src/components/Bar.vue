c729ea6e5ba43369e4657553a42df2d8370ab18d
Hana <momenthana@kakao.com>
2020-05-13 14:28:26 +0900

    컴포넌트 생성

---
+++ src/components/Bar.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app-bar
+      absolute
+      color="#ff6f61"
+      dark
+      prominent
+      src=""
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Materials</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>1</v-tab>
+          <v-tab>2</v-tab>
+          <v-tab>3</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: 'Bar'
+  }
+</script>
