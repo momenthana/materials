53ff2d2f48a647a9e0d6d048822f7b8c28a125ea
Hana <momenthana@kakao.com>
2020-05-29 16:15:33 +0900

    Dark mode support

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,12 @@ export default {
     List
   },
 
-  data: () => ({
-    //
-  }),
+  mounted () {
+    const dark = window.matchMedia('(prefers-color-scheme: dark)')
+    this.$vuetify.theme.dark = dark.matches
+    dark.addEventListener('change', () => {
+      this.$vuetify.theme.dark = dark.matches
+    })
+  }
 };
 </script>
