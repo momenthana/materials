c8e9d4c58bf911de18352e10c6ea18409b1e2a83
Hana <momenthana@kakao.com>
2020-06-16 15:08:00 +0900

    yarn add koa

--- svr/index.js
+++ svr/index.js
@@ -1,1 +1,10 @@
-console.log('server');
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = 'Hello, Hana!';
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening to port 3000');
+});
