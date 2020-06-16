d84fee3507e597dfb5316133c4edb2e58a7541dc
Hana <momenthana@kakao.com>
2020-06-16 15:12:34 +0900

    yarn add koa-router

--- svr/index.js
+++ svr/index.js
@@ -1,9 +1,15 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
 
-app.use(ctx => {
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
     ctx.body = 'Hello, Hana!';
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000, () => {
     console.log('Server is listening to port 3000');
