0e63c5085d9739035a4b16736bf4ff1d024b08b8
Hana <momenthana@kakao.com>
2020-06-18 11:34:48 +0900

    vue add router

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
   <v-app>
-    <Bar />
-    <Nav />
-
-    <v-main>
-      <Node v-if="$store.state.group" />
-      <Group v-else />
-    </v-main>
+    <router-view />
   </v-app>
 </template>
 
 <script>
-import Bar from "./components/Bar";
-import Nav from "./components/Nav";
-import Group from "./components/Group";
-import Node from "./components/Node";
-
 export default {
   name: "App",
-
-  components: {
-    Bar,
-    Nav,
-    Group,
-    Node
-  },
 
   mounted() {
     const dark = window.matchMedia("(prefers-color-scheme: dark)");
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <Bar />
+    <Nav />
+
+    <v-main>
+      <Node v-if="$store.state.group" />
+      <Group v-else />
+    </v-main>
+  </div>
+</template>
+
+<script>
+import Bar from '@/components/Bar.vue'
+import Nav from '@/components/Nav.vue'
+import Node from '@/components/Node.vue'
+import Group from '@/components/Group.vue'
+
+export default {
+  name: 'Home',
+
+  components: {
+    Bar,
+    Nav,
+    Node,
+    Group
+  }
+}
+</script>
