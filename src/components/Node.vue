35ecd092eb4475c5f24eb9238ed3e09bb69069bc
Hana <momenthana@kakao.com>
2020-06-19 14:57:06 +0900

    QR 코드 추가

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -14,11 +14,7 @@
           <v-card-subtitle>{{ item.description }}</v-card-subtitle>
 
           <v-card-actions>
-            <v-btn
-              color="#ff6f61"
-              dark
-              @click="$store.state.node = item.node"
-            >보기</v-btn>
+            <v-btn color="#ff6f61" dark @click="$store.state.node = item.node, dialog = true">보기</v-btn>
 
             <v-btn text>담당자 {{ item.name }}</v-btn>
 
@@ -45,16 +41,28 @@
     </v-row>
 
     <v-row justify="center">
-      <v-dialog v-model="$store.state.node" :width="$vuetify.breakpoint.smAndDown ? '75%' : '50%'">
+      <v-dialog v-model="dialog" :width="$vuetify.breakpoint.smAndDown ? '75%' : '50%'">
         <v-card>
-          <v-card-title class="headline">Title</v-card-title>
+          <v-img
+            src="https://lh3.googleusercontent.com/hgR_vN46zGWDhTd1j9zbmCFdXty6VONuNcBqbh_vO9Ci2RuBGJVxQkO2d3Zbz9LRuup0WeHggzGv=w604-h206-p"
+            height="100px"
+          ></v-img>
 
-          <v-card-text>Text</v-card-text>
+          <v-card-title class="headline">
+            {{ $store.state.node }}
+            <v-btn class="ml-5" text>담당자 김하나</v-btn>
+            <v-spacer></v-spacer>
+
+            <vue-qrcode
+              :value="`${url}/${$store.state.group}/${$store.state.node}`"
+              :options="{ errorCorrectionLevel: 'H' }"
+            />
+          </v-card-title>
 
           <v-card-actions>
             <v-spacer></v-spacer>
 
-            <v-btn color="#ff6f61" dark @click="$store.state.node = null">닫기</v-btn>
+            <v-btn color="#ff6f61" dark @click="dialog = false">닫기</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -63,10 +71,18 @@
 </template>
 
 <script>
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+
 export default {
   name: "Node",
 
+  components: {
+    VueQrcode
+  },
+
   data: () => ({
+    url: process.env.VUE_APP_BASE_URL,
+    dialog: false,
     items: [
       {
         node: "1",
