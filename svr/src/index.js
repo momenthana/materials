bed07153ff9f44b907ff4e7cd8068cb2827e28b9
Hana <momenthana@kakao.com>
2020-06-29 14:06:25 +0900

    mongoose init

--- svr/src/index.js
+++ svr/src/index.js
@@ -1,14 +1,26 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const mongoose = require('mongoose')
 
 const app = new Koa()
 const router = new Router()
 const root = require('./root')
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(process.env.MONGO, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}).then(() => {
+	console.log('Successfully connected to mongodb');
+}).catch(e => {
+	console.error(e);
+})
 
 router.use('', root.routes())
 
 app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is listening to port ${process.env.PORT}`)
+	console.log(`Server is listening to port ${process.env.PORT}`)
 })
