f18679c512b377b4066c22a3cb1e4c51e2cc24e8
Hana <momenthana@kakao.com>
2020-08-21 08:11:39 +0900

    그룹 생성 api 연결

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -3,25 +3,19 @@
     <v-dialog v-model="$store.state.dialog" :width="$vuetify.breakpoint.smAndDown ? '75%' : '50%'">
       <v-card>
         <v-card-title>
-          <span class="headline">Title</span>
+          <span class="headline" v-text="$store.state.group ? '물품' : '그룹'"></span>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Label"></v-text-field>
+              <v-col cols="12" sm="7" md="8">
+                <v-text-field v-model="title" label="표제" color="#ff6f61"></v-text-field>
               </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Label"></v-text-field>
+              <v-col cols="12" sm="5" md="4">
+                <v-text-field v-model="description" label="담당자" color="#ff6f61"></v-text-field>
               </v-col>
-              <v-col cols="12" md="4">
-                <v-text-field label="Label"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-text-field label="Label"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-text-field label="Label"></v-text-field>
+              <v-col cols="12">
+                <v-text-field v-model="name" label="설명" color="#ff6f61"></v-text-field>
               </v-col>
             </v-row>
           </v-container>
@@ -29,9 +23,37 @@
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="#ff6f61" text @click="$store.state.dialog = false">취소</v-btn>
-          <v-btn color="#ff6f61" dark @click="$store.state.dialog = false">추가</v-btn>
+          <v-btn color="#ff6f61" dark @click="$store.state.dialog = false, axios()">추가</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
   </v-row>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    title: null,
+    description: null,
+    name: null,
+  }),
+
+  methods: {
+    axios: function () {
+      if (!this.$store.state.group) {
+        axios
+          .post("//" + this.$store.state.server + "/group", {
+            title: this.title,
+            description: this.description,
+            name: this.name,
+          })
+          .then((res) => {
+            console.log(res.data);
+          });
+      }
+    },
+  },
+};
+</script>
