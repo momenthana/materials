b1c242d68b91f46a2f114848d5c1cc7cda8b40ba
Hana <momenthana@kakao.com>
2020-08-26 21:00:45 +0900

    색상 선택 기능 추가

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,13 +2,22 @@
   <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
     <v-list-item class="mt-12">
       <v-list-item-content>
-        <v-text-field
-          solo-inverted
-          v-model="$store.state.color"
-          placeholder="Color"
-          prepend-inner-icon="mdi-format-color-fill"
-          :color="$store.state.color"
-        ></v-text-field>
+        <v-menu v-model="menu" :close-on-content-click="false">
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              readonly
+              v-bind="attrs"
+              v-on="on"
+              solo-inverted
+              v-model="$store.state.color"
+              placeholder="Color"
+              prepend-inner-icon="mdi-format-color-fill"
+              :color="$store.state.color"
+            ></v-text-field>
+          </template>
+
+          <v-color-picker light v-model="$store.state.color" elevation="15"></v-color-picker>
+        </v-menu>
         <v-text-field
           solo-inverted
           v-model="$store.state.server"
@@ -32,7 +41,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "Nav",
@@ -42,32 +51,34 @@ export default {
   }),
 
   created() {
-    this.axios()
+    this.axios();
   },
 
   watch: {
-    '$store.state.color': function () {
-      localStorage.setItem('color', this.$store.state.color)
-      this.axios()
+    "$store.state.color": function () {
+      localStorage.setItem("color", this.$store.state.color);
+      this.axios();
     },
-    '$store.state.server': function () {
-      localStorage.setItem('server', this.$store.state.server)
-      this.axios()
+    "$store.state.server": function () {
+      localStorage.setItem("server", this.$store.state.server);
+      this.axios();
     },
-    '$store.state.token': function () {
-      localStorage.setItem('token', this.$store.state.token)
-      this.axios()
-    }
+    "$store.state.token": function () {
+      localStorage.setItem("token", this.$store.state.token);
+      this.axios();
+    },
   },
 
   methods: {
     axios: function () {
-      this.$store.state.online = false
-      axios.get('//' + this.$store.state.server + '/').then((res) => {
-        this.$store.state.online = res.status == 200 ? true : false
-      })
-      .catch(() => {})
-    }
-  }
+      this.$store.state.online = false;
+      axios
+        .get("//" + this.$store.state.server + "/")
+        .then((res) => {
+          this.$store.state.online = res.status == 200 ? true : false;
+        })
+        .catch(() => {});
+    },
+  },
 };
 </script>
